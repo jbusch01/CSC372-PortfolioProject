c4e59984677571b4e5f1dd201e9c5266d489b3d9
jbusch01 <jonrbusch@gmail.com>
2025-06-06 13:41:01 -0600

    Updated variables in object class

--- src/Student.java
+++ src/Student.java
@@ -1,70 +1,30 @@
 public class Student {
-    private String firstName;
-    private String lastName;
-    private String streetAddress;
-    private String city;
-    private String state;
-    private int zipCode;
+    private String name;
+    private String address;
     private double gpa;
     private int studentID;
 
-    public Student(String firstName, String lastName, String streetAddress, String city, String state, int zipCode, double gpa, int studentID) {
-        this.firstName = firstName;
-        this.lastName = lastName;
-        this.streetAddress = streetAddress;
-        this.city = city;
-        this.state = state;
-        this.zipCode = zipCode;
+    public Student(String name, String address, double gpa, int studentID) {
+        this.name = name;
+        this.address = address;
         this.gpa = gpa;
         this.studentID = studentID;
     }
 
-    public String getFirstName() {
-        return firstName;
+    public String getName() {
+        return name;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-    
-    public String getLastName() {
-        return lastName;
+    public void setName(String name) {
+        this.name = name;
     }
 
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
+    public String getAddress() {
+        return address;
     }
 
-    public String getStreetAddress() {
-        return streetAddress;
-    }
-
-    public void setStreetAddress(String streetAddress) {
-        this.streetAddress = streetAddress;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getState() {
-        return state;
-    }
-
-    public void setState(String state) {
-        this.state = state;
-    }
-
-    public int getZipCode() {
-        return zipCode;
-    }
-
-    public void setZipCode(int zipCode) {
-        this.zipCode = zipCode;
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public double getGPA() {
